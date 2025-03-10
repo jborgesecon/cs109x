51f4a5c944685a8889692d1e97ae9cb5564452ba
joao <jg.gabrielborges@gmail.com>
2025-03-10 16:22:14 -0300

    beggining: class 2

---
+++ classes/class2.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+def show_class2():
+    st.markdown(
+        """
+## Pre-reading:
+- cap 3.2
+- cap 7.1
+"""
+    )
+    st.markdown(
+        """
+## Multiple and Polynomial Linear Regression (ch. 3, 7)
+
+
+"""
+    )
+    return
--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,5 @@
 import classes
+import classes.class2
 import classes.introduction
 import classes.cheat_sheet
 import classes.class1
@@ -6,5 +7,6 @@ import classes.class1
 classes_dict = {
     'Introduction': classes.introduction.show_introduction,
     'Class 1: Simple Regression': classes.class1.show_class1,
+    'Class 2: Multiple and Polynomial Regression': classes.class2.show_class2,
     'Cheat Sheets': classes.cheat_sheet.show_cheatsheets
 }
